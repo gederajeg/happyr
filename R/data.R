5718f8f462e2ff96f3377f7d60787fddcb31acac
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2018-07-03 11:33:20 +1000

    add the data and sysdata, including the documentation.

---
+++ R/data.R
@@ -0,0 +1,44 @@
+#' The metaphor data frame
+#'
+#' The metaphor data for TTR analysis in Ch. 5 & Ch. 6, and for the MDCA analysis in Ch. 7.
+"phd_data_metaphor"
+
+#' Distinctive collocates data
+#'
+#' The input data for collocational analysis (4-window span collocates of the synonyms).
+"colloc_input_data"
+
+#' Interrater data for conceptual metaphors
+#'
+#' The input data for interrater agreement trial for the conceptual metaphors.
+#' The data include classifications in the pre-discussion and post discussion rounds.
+"df_cm"
+
+#' Interrater data for metaphorical usages of the pattern (1st round)
+#'
+#' The input data for interrater agreement trial for determining whether the constructional patterns of the happiness words are metaphorical or not.
+#' The data include classifications in the pre-discussion round.
+"df_meta_use_1st"
+
+#' Interrater data for metaphorical usages of the pattern (2nd round)
+#'
+#' The input data for interrater agreement trial for determining whether the constructional patterns of the happiness words are metaphorical or not.
+#' The data include classifications in the post-discussion round.
+"df_meta_use_2nd"
+
+#' Interrater data for the constructional patterns
+#'
+#' The input data for interrater agreement trial for determining the constructional patterns of the happiness words in their concordance lines.
+#' The data include classifications in the pre-discussion round only since we already arrived at a nearly perfect agreement before the discussion.
+"df_cxn_pattern"
+
+#' Interrater data for the top constructional patterns
+#'
+#' The input data for \emph{Figure 3-1} in Rajeg (2018).
+#' The barplot shows the distribution of constructional patterns during interrater agreement trial whose frequency of occurrences are at least five (5).
+#' @references Rajeg, G. P. W. (2018). \emph{Metaphorical profiles and near-synonyms: A corpus-based study of Indonesian words for happiness}. PhD Thesis. Monash University. Melbourne, Australia.
+"top_cxn_data"
+
+
+
+
