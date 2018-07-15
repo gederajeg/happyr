7c80df0e889c4d0a1b77aca0ef5e42cec0c9ceeb
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2018-07-15 16:00:37 +1000

    add description to `scaps()` function argument

--- R/scaps.R
+++ R/scaps.R
@@ -2,13 +2,13 @@
 #' Small-capital
 #'
 #' @description A utility function to turn lower case character into small capitals
-#' @param concept
+#' @param x A character strings to be turned into small capitals
 #'
 #' @return A character vector
 #' @export
 #'
 #' @examples
 #' scaps("happiness is a desired goal")
-scaps <- function(concept = NULL) {
-  paste("<span style = 'font-variant:small-caps;'>", concept, "</span>", sep = "")
+scaps <- function(x = NULL) {
+  paste("<span style = 'font-variant:small-caps;'>", x, "</span>", sep = "")
 }
