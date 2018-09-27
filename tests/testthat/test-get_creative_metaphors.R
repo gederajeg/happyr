27406aaa5623673f75aaafc00a1e553595cb7226
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2018-09-27 10:11:50 +1000

    add a function with shorter name for retrieving the creative metaphor in Chapter 6

---
+++ tests/testthat/test-get_creative_metaphors.R
@@ -0,0 +1,18 @@
+context("test-get_creative_metaphors.R")
+
+test_that("`get_creative_metaphors()` throws error message when `df_ttr_out` argument is NULL", {
+  expect_error(get_creative_metaphors(min_token = 5, top_n_limit = 10), "The `df_ttr_out` argument is NULL")
+})
+
+ttr_metaphor <- ttr(df = phd_data_metaphor,
+                    schema_var = "metaphors",
+                    lexunit_var = "lu",
+                    float_digits = 2)
+creative_metaphors <- get_creative_metaphors(df_ttr_out = ttr_metaphor,
+                                                      min_token = 3,
+                                                      top_n_limit = 10)
+
+test_that("the correct columns output of `get_cic_meta_lu()`", {
+  expect_output(str(creative_metaphors), "6 variables", all = FALSE, perl = TRUE)
+  expect_output(str(creative_metaphors), "\\$ ((type_(lu|per_token_lu))|(perc_)?(token|type_lu))", all = TRUE, perl = TRUE)
+})
